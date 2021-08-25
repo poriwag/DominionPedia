d81c822f4a8b590fb1502032b7b7b7fbe56e3a3b
Billy Pak <pak.billy@gmail.com>
2021-08-24 17:57:52 -0700

    initial commit

--- DominionPedia/Utilities/Constants.swift
+++ DominionPedia/Utilities/Constants.swift
@@ -5,4 +5,18 @@
 //  Created by billy pak on 7/23/21.
 //
 
-import Foundation
+import UIKit
+
+enum SFSymbols {
+    
+    static let cardList = UIImage(systemName: "list.bullet")
+    static let expansionBox = UIImage(systemName: "archivebox.fill")
+    static let settings = UIImage(systemName: "gearshape")
+}
+
+enum Images {
+    static let dominionLogo = UIImage(named: "DominionLogo")
+    static let defaultImage = UIImage(named: "CardBack")
+}
+
+
