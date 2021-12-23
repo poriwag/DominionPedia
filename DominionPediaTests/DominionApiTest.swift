eacd3959d0bf302792dd4127b714f01d2689fed9
Billy pak <pak.billy@gmail.com>
2021-12-23 09:43:04 -0800

    Tests added

---
+++ DominionPediaTests/DominionApiTest.swift
@@ -0,0 +1,46 @@
+//
+//  DominionApiTest.swift
+//  DominionPediaTests
+//
+//  Created by billy pak on 12/23/21.
+//
+
+import XCTest
+@testable import DominionPedia
+
+class DominionApiTest: XCTestCase {
+    
+    var sut: URLSession!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = URLSession(configuration: .default)
+    }
+    
+    override func tearDownWithError() throws {
+        sut = nil
+        try super.tearDownWithError()
+    }
+    
+    func testApiCallCompletes() throws {
+      // given
+      let urlString = "https://dominioncardapi.pythonanywhere.com/cards/"
+      let url = URL(string: urlString)!
+      let promise = expectation(description: "Completion handler invoked")
+      var statusCode: Int?
+      var responseError: Error?
+
+      // when
+      let dataTask = sut.dataTask(with: url) { _, response, error in
+        statusCode = (response as? HTTPURLResponse)?.statusCode
+        responseError = error
+        promise.fulfill()
+      }
+      dataTask.resume()
+      wait(for: [promise], timeout: 5)
+
+      // then
+      XCTAssertNil(responseError)
+      XCTAssertEqual(statusCode, 200)
+    }
+}
